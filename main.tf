1da54cf8bfbce20c299973ba55364bb2bb8b32fb
heman chandra <hemanchandra701@gmail.com>
2026-06-14 14:26:26 +0530

    github-actions

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "runner" {
     instance_type           = "t3.small"
     vpc_security_group_ids  = [aws_security_group.main.id]
     subnet_id              = local.public_subnet_id
-    associate_public_ip_address = true
     
 
     root_block_device {
