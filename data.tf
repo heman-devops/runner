48678389301597fb62d436255f088da071722ea2
heman chandra <hemanchandra701@gmail.com>
2026-06-14 13:58:46 +0530

    github-actions

--- data.tf
+++ data.tf
@@ -17,9 +17,12 @@ data "aws_ami" "heman" {
     }
 }
 
-# data "aws_ssm_parameter" "private_subnet_ids" {
-#   name = "/${var.project}/${var.environment}/private_subnet_ids"
-# }
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
+}
 
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
 
 
--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "runner" {
     ami                     = local.ami_id
     instance_type           = "t3.small"
     vpc_security_group_ids  = [aws_security_group.main.id]
-    subnet_id              = "subnet-085fcd36b823a278f"
+    subnet_id              = local.private_subnet_id
+    
 
     root_block_device {
       volume_size = 50
@@ -23,6 +24,7 @@ resource "aws_instance" "runner" {
 resource "aws_security_group" "main" {
   name        = "${local.common_name}-runner-sg"
   description = "Allow all inbound and outbound traffic"
+  vpc_id                   = local.vpc_id
 
   ingress {
     description = "Allow all inbound traffic"
